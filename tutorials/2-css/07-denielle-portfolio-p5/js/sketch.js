f9b330fa59db928ea79b95707760f2054bbd2aec
spenielle <denielle.sambilay@sjsu.edu>
2026-03-22 21:15:25 -0700

    update

---
+++ tutorials/2-css/07-denielle-portfolio-p5/js/sketch.js
@@ -0,0 +1,41 @@
+
+let canvas;
+let xPos = 0;
+let yPos = 0;
+let easing = 0.09;
+
+function setup (){
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0,0);
+    canvas.style("z-index", -2);
+}
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw (){
+    clear();
+
+
+    xPos = xPos + ((mouseX - xPos) * easing);
+    yPos = yPos + ((mouseY - yPos) * easing);
+
+    drawThing(xPos, yPos);
+
+}
+
+function mouseMoved(){
+    background(100);
+    drawThing(mouseX, mouseY);
+   
+}
+
+function drawThing(_x, _y){
+    //strokeWeight(0);
+    // fill(random(200, 255), random(200, 255), random(200, 255));
+    // ellipse(_x, _y, 30, 30);
+    noStroke();
+    fill(random(200, 255), random(200, 255), random(200, 255));
+    rect(_x, _y, 30, 30);
+}
